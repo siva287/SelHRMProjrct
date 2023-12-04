e6b165121754759d9991334d5d49c08374800e66
siva287 <sivavinjam96@gmai.com>
2023-12-04 11:11:01 +0530

    new file modified

--- testCases/new_file.py
+++ testCases/new_file.py
@@ -1,4 +1,18 @@
-hjsdfiuiJSF
+# serv_obj=Service("C:/Users/konda/Drivers/chromedriver.exe")
+# driver=webdriver.Chrome(service=serv_obj)
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+
+def test_me():
+    driver = webdriver.Chrome()
+    driver.get("https://www.aptransport.org/")
+    driver.implicitly_wait(10)
+    print(driver.title)
+    mh = driver.find_element(By.LINK_TEXT, "LICENCE")
+    ac = ActionChains(driver)
+    ac.move_to_element(mh).click().perform()hjsdfiuiJSF
 huihsfjnWE
 IHUYESE
 JIHSFGE
